7bb92aa708c27be6e915a4c661b4b9d9869aba73
McCrearyD <mccreary@dyllan.ai>
2021-05-01 22:57:28 -0700

    start chunk manager with dummy storage provider/cache

--- hub/core/chunk_engine/manager.py
+++ hub/core/chunk_engine/manager.py
@@ -2,9 +2,12 @@ import numpy as np
 
 from hub.core.chunk_engine.generator import chunk
 
-# TODO: add compression functions to _compressor_function_map
+# TODO: add compression functions to _compressor_function_map & remove dummy
 # each compressor is a function that takes in a chunk (bytes) & returns the compressed chunk (bytes).
-_compressor_function_map = {None: lambda x: x}
+_compressor_function_map = {
+    None: lambda x: x,
+    "dummy_compressor": lambda x: x[: (len(x) // 2)],
+}
 
 
 def _assert_valid_shape(a: np.array):
@@ -13,7 +16,26 @@ def _assert_valid_shape(a: np.array):
 
 # TODO: delete dummy storage provider
 class DummyStorageProvider:
-    pass
+    def __init__(self, url: str):
+        self.url = url
+        self.storage = {}
+
+    def get_available_bytes(self):
+        """
+        Check the amount of available bytes that this StorageProvider has left to store.
+        """
+
+        return 9999999999
+
+    def write(self, key: str, b: bytes):
+        """
+        Store bytes `b` under `key`.
+        """
+
+        if key in self.storage:
+            raise Exception("key %s already exists." % key)
+
+        self.storage[key] = b
 
 
 class ChunkManager:
@@ -21,24 +43,52 @@ class ChunkManager:
     Manage the chunks for a single tensor.
 
     Args:
+        storage_url(str): String with a url & path component.
+            Format: [storage_provider_url]/path
+            Example: s3://some-bucket/store/
         compressor(str): String for the compressor to be used.
     """
 
-    def __init__(self, compressor=None, chunk_size=2048, pickle=False):
+    def __init__(
+        self,
+        storage_url: str,
+        compressor: str = None,
+        chunk_size: int = 2048,
+        pickle: bool = False,
+    ):
         # TODO: change default chunk_size
         self.chunk_size = chunk_size
 
         # TODO: compressor selection docstring
-        self.compressor = compressor
+        self.compress = _compressor_function_map[compressor]
 
-        # TODO: replace cache_chain with actual StorageProviders
+        # TODO: replace cache_chain/storage with actual StorageProviders
         # cache_chain is an ordered list of StorageProviders, the first of which is the preferred
         # cache method, & the last is the last resort cache method.
-        self.cache_chain = [DummyStorageProvider()]
+        self.storage_provider = DummyStorageProvider(storage_url)
+        self.cache_chain = [self.storage_provider]
 
         self.pickle = pickle
         if self.pickle:
             raise NotImplementedError("Pickle support is not available yet.")
+
+    def get_cache(self, num_bytes: int):
+        """
+        Goes through `self.cache_chain` & gets the first one available with sufficient space.
+
+        Args:
+            num_bytes(int): Number of bytes needed to cache.
+
+        Returns:
+            cache(StorageProvider): StorageProvider that can be used to cache incomplete chunks.
+        """
+
+        for cache in self.cache_chain:
+            if num_bytes <= cache.get_available_bytes():
+                return cache
+
+        # TODO: failure to get cache from cache_chain in exceptions.py
+        raise Exception("No cache space available.")
 
     def write(self, data: np.array):
         # TODO: chunkmanager support for list(np.array)
@@ -53,9 +103,25 @@ class ChunkManager:
             data_bytes = data.tobytes()
 
         # TODO: get previous chunk's num bytes
-        previous_num_bytes = 0
+        previous_num_bytes = None
         for uncompressed_chunk_bytes, relative_chunk_index in chunk(
             data_bytes, previous_num_bytes, self.chunk_size
         ):
-            print(data_bytes)
-            exit()
+            uncompressed_length = len(uncompressed_chunk_bytes)
+
+            if uncompressed_length < self.chunk_size:
+                # if this chunk is incomplete, cache it
+                cache = self.get_cache(uncompressed_length)
+
+                cache.write("incomplete_chunk", uncompressed_chunk_bytes)
+                break
+
+            if relative_chunk_index == 0:
+                raise NotImplementedError()
+
+            # compress & store full chunk
+            compressed_chunk_bytes = self.compress(uncompressed_chunk_bytes)
+            key = "chunk_%i" % relative_chunk_index
+            self.storage_provider.write(key, compressed_chunk_bytes)
+
+        # TODO: index map
--- hub/core/chunk_engine/tests/test_chunk_manager.py
+++ hub/core/chunk_engine/tests/test_chunk_manager.py
@@ -1,5 +1,12 @@
 from hub.core.chunk_engine import ChunkManager
+from hub.core.chunk_engine.tests.util import get_random_shaped_array
 
 
 def test_write():
-    pass
+    # TODO: test configs
+    cm = ChunkManager(
+        "test_storage_provider", compressor="dummy_compressor", chunk_size=256
+    )
+
+    a = get_random_shaped_array((10, 10), dtype="int32", fixed=True)
+    cm.write(a)
