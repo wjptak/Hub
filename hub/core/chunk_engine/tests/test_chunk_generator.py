6dbd8438d5855454b85d7e86e9df72b2b2957d8e
McCrearyD <mccreary@dyllan.ai>
2021-05-03 14:57:12 -0700

    start parametrize

--- hub/core/chunk_engine/tests/test_chunk_generator.py
+++ hub/core/chunk_engine/tests/test_chunk_generator.py
@@ -4,17 +4,23 @@ from hub.core.chunk_engine.generator import chunk_along_all_axes
 
 from util import get_random_shaped_array
 
+import pytest
 
-def test_perfect_fit():
-    dtype = "int32"
-    chunk_size = 4096
 
-    # must be batched!! (if not batched, need to add an axis of size 1)
-    batched_samples = get_random_shaped_array((100, 1000), dtype, fixed=True)
+test_data = [
+    ("int32", 4096, (100, 100), True),
+]
 
-    # since batched_samples is of shape (N=100, D=1000), & chunk_size=5000:
-    # the output chunks should be one-dimensional (D=5000)
+
+@pytest.mark.parametrize(
+    "dtype,chunk_size,data_max_shape,data_is_fixed_shape", test_data
+)
+def test_perfect_fit(dtype, chunk_size, data_max_shape, data_is_fixed_shape):
+    assert len(data_max_shape) > 1, "Data should be batched for this test."
+
+    batched_samples = get_random_shaped_array(
+        data_max_shape, dtype, fixed=data_is_fixed_shape
+    )
 
     for chunk, chunk_position in chunk_along_all_axes(batched_samples, chunk_size):
-
-        pass
+        break
