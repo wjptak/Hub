594ef0e18e92d6d739d28b6d438ee1b63ed33e40
McCrearyD <mccreary@dyllan.ai>
2021-05-02 18:48:58 -0700

    add helper function to generate randomly sized arrays

--- hub/core/chunk_engine/tests/util.py
+++ hub/core/chunk_engine/tests/util.py
@@ -2,6 +2,8 @@ from copy import deepcopy
 
 import numpy as np
 
+from typing import Tuple
+
 
 def make_dummy_byte_array(length: int) -> bytearray:
     """Generate a random bytearray of the provided length."""
@@ -27,3 +29,30 @@ def get_random_partial(chunk_size: int) -> int:
 def assert_valid_chunk(chunk: bytes, chunk_size: int):
     assert len(chunk) > 0
     assert len(chunk) <= chunk_size
+
+
+def get_random_shaped_array(
+    random_max_shape: Tuple[int], dtype: str, fixed: bool = False
+):
+    # TODO docstring for random shaped array
+
+    # get a random shape
+    if fixed:
+        dims = random_max_shape
+    else:
+        dims = []
+        if type(random_max_shape) != int:
+            for max_dim in random_max_shape:
+                dims.append(np.random.randint(1, max_dim + 1))
+
+    if "int" in dtype:
+        low = np.iinfo(dtype).min
+        high = np.iinfo(dtype).max
+        a = np.random.randint(low=low, high=high, size=dims, dtype=dtype)
+    elif "float" in dtype:
+        a = np.random.random_sample(size=dims).astype(dtype)
+    elif "bool" in dtype:
+        a = np.random.uniform(size=dims)
+        a = a > 0.5
+
+    return a
