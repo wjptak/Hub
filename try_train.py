790abeb8b914c96fa10c4b7d32bdc49cd58a4194
McCrearyD <mccreary@dyllan.ai>
2021-05-27 20:55:56 +0000

    add read experiment

---
+++ try_train.py
@@ -0,0 +1,17 @@
+from hub import Dataset
+
+from hub.core.storage import MemoryProvider, S3Provider, LocalProvider
+from hub.util.cache_chain import get_cache_chain
+
+dataset_name = "asl_alphabet"
+s3 = S3Provider("s3://hub-2.0-datasets/%s" % dataset_name)
+storage = get_cache_chain(
+    [MemoryProvider(dataset_name), s3],
+    [
+        256 * 1024 * 1024,
+    ],)
+
+ds = Dataset(mode="r", provider=storage)
+
+print(ds.tensors.keys())
+print(ds["O"][0].numpy())
