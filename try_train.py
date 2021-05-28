d12e2a891ba8bb71ad669ec13a337b740cf75e1b
McCrearyD <mccreary@dyllan.ai>
2021-05-28 01:04:44 +0000

    get label_names

--- try_train.py
+++ try_train.py
@@ -1,8 +1,11 @@
+import tqdm
 import matplotlib.pyplot as plt
 from hub import Dataset
 
 from hub.core.storage import MemoryProvider, S3Provider, LocalProvider
 from hub.util.cache_chain import get_cache_chain
+
+from hub.core.chunk_engine import read_tensor_meta
 
 dataset_name = "asl_alphabet"
 s3 = S3Provider("s3://hub-2.0-datasets/%s" % dataset_name)
@@ -14,8 +17,10 @@ storage = get_cache_chain(
 )
 
 ds = Dataset(mode="r", provider=storage)
+meta = read_tensor_meta("images", ds.provider)
+label_names = meta["label_names"]  # TODO: API should do this, something like ds["labels"][0].name would be very convenient
 
-print("train:")
-for x, t in ds:
-    print(t)
+for sample in tqdm.tqdm(ds, total=len(ds), desc="iterating through ds"):
+    x, t = sample["images"].numpy(), sample["labels"].numpy()
+    label_name = label_names[t[0][0]]
     break
