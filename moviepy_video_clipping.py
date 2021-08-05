96f649aefd08d57827bfd8c7358cc0888ffdabf4
McCrearyD <mccreary@dyllan.ai>
2021-08-05 12:14:56 -0700

    add function for saving as frames

--- moviepy_video_clipping.py
+++ moviepy_video_clipping.py
@@ -2,6 +2,8 @@ from moviepy.editor import *
 
 
 def clip(src_path, dest_dir, seconds_per_clip: int):
+    """Saves the video at `src_path` as clips of max length `seconds_per_clip` inside `dest_dir`."""
+
     video = VideoFileClip(src_path)
     nclips = int(video.duration / seconds_per_clip)
 
@@ -9,7 +11,20 @@ def clip(src_path, dest_dir, seconds_per_clip: int):
     for i in range(nclips):
         print(f"creating clip {i}/{nclips}...")
         dest_path = os.path.join(dest_dir, 'clip_{}.mp4'.format(i))
-        sub = video.subclip(i * seconds_per_clip, (i + 1) * seconds_per_clip)
+
+        start = i * seconds_per_clip
+        end =  min((i + 1) * seconds_per_clip, video.duration)
+
+        sub = video.subclip(start, end)
         sub.write_videofile(dest_path)
 
-clip("nasa.mp4", "OUTPUT_CLIPS", 10)
+
+def images(src_path, dest_dir):
+    """Saves the video at `src_path` as single-frame images inside `dest_dir`."""
+
+    video = VideoFileClip(src_path)
+    dest_path = os.path.join(dest_dir, "frame%04d.jpeg")
+    video.write_images_sequence(dest_path)
+
+# clip("nasa.mp4", "OUTPUT_CLIPS", 90)
+# images("nasa.mp4", "OUTPUT_FRAMES")
